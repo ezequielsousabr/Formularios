03a71bb9ae20c0a007a610676fd8bc03fe2f556a
Ezequiel L Sousa <ezequiel.sousa@yahoo.com>
2023-01-20 12:25:09 -0300

    Criando validação do formulario

--- FormulariosContato/001/validador.js
+++ FormulariosContato/001/validador.js
@@ -1,1 +1,23 @@
-// VALIDADOR DE FORMULARIOS
+/* Armazenando dados importantes */
+
+const nome      = document.querySelector('.nome');
+const email     = document.querySelector('.email');
+const telefone  = document.querySelector('.telefone');
+const cidade    = document.querySelector('.cidade');
+const estado    = document.querySelector('.estado');
+const descricao = document.querySelector('#descricao');
+
+const dados  = [
+    '.nome',
+    '.email',
+    '.telefone',
+    '.cidade',
+    '.estado'
+];
+
+/* Verificar se todos os campos estão preenchidos
+corretamente. */
+
+if(dados.value === ""){
+    alert("Preencha o formulário!");
+}
