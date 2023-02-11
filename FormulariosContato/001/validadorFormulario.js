c2a3c3b717315b1d5a51d9e82787536ab88ba790
Ezequiel L Sousa <ezequiel.sousa@yahoo.com>
2023-02-11 12:31:10 -0300

    Criando validação de formulário
    Melhorando a validação de formulário.

--- FormulariosContato/001/validadorFormulario.js
+++ FormulariosContato/001/validadorFormulario.js
@@ -1,51 +1,51 @@
-/*
-Criando uma constante para selecionar todos os campos de entrada
-do usuário. */
+document.querySelector("#enviar").addEventListener("click", function(event) {
+    event.preventDefault();
 
-const in_inputs  = document.querySelectorAll( [
+    var nome = document.querySelector(".nome").value;
+    var email = document.querySelector(".email").value;
+    var telefone = document.querySelector(".telefone").value;
+    var cidade = document.querySelector(".cidade").value;
+    var descricao = document.querySelector("#descricao").value;
+    var empresa = document.querySelector("input[type=text]").value;
 
-    '.nome','.email','.telefone','.cidade','.estado'
+    var validacao = true;
 
-]);
-
-/*
-Verificar todos as condições de entradas dos campos inputs
-para validação de dados do usuário. */
-
-if((in_inputs[0].value.length === "") || (in_inputs[0].value.length <= 4)){
-    alert("Preencha seu nome completo corretamente!");
-    in_inputs[0].focus();
-}
-
-if(in_inputs[1].value.length === ""){
-    alert("Preencha o campo email corretamente!");
-}
-
-if(in_inputs[3].value.length >= 4){
-    in_inputs[4].removeAttribute('disabled');
-}else{
-    in_inputs[4].setAttribute('disabled', '');
-}
-
-/*
-Inserir de forma automática todos os ufs de estados.
-Cada uf deve estar dentro de da sua tag options. */
-
-var ufList = ["sp","mg","rs","rn","es","ba","ro"];
-
-console.log(ufList);
-
-function ufOptions(ufList){
-
-    let estados = ufList;
-
-    let options = `<options>${estados}</options`;
-
-    console.log(options);
-
-    for(var cont = options; options <= 27; i++){
-        console.log(options + 'Listando');
+    if (!nome) {
+        alert("Por favor, preencha o campo Nome.");
+        validacao = false;
     }
+
+    if (!validarEmail(email)) {
+        alert("Por favor, preencha o campo E-mail com um endereço de e-mail válido.");
+        validacao = false;
+    }
+
+    if (!validarTelefone(telefone)) {
+        alert("Por favor, preencha o campo Telefone com um número de telefone válido.");
+        validacao = false;
+    }
+
+    if (!cidade) {
+        alert("Por favor, preencha o campo Cidade.");
+        validacao = false;
+    }
+
+    if (!descricao) {
+        alert("Por favor, preencha o campo Conte-nos o que você precisa!");
+        validacao = false;
+    }
+
+    if (validacao) {
+        alert("Formulário enviado com sucesso!");
+    }
+});
+
+function validarEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
 }
 
-ufOptions(ufList);
+function validarTelefone(telefone) {
+    var re = /^\d{2}\s\d{4,5}\-\d{4}$/;
+    return re.test(telefone);
+}
