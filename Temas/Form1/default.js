b0775d13a72b954e6c73b4d078c45dd288b3b2c3
Ezequiel L Sousa <ezequielsousabr@yahoo.com>
2023-03-08 19:53:41 -0300

    Alterações
    Pagina de resposta 200 Ok adicionada

---
+++ Temas/Form1/default.js
@@ -0,0 +1,5 @@
+/*
+Formulário devera mostrar texto de enviado com sucesso do lado esquerdo
+da tela quando o usuário clicar em enviar.
+Se o usuário não preencher todas as infomações corretamente, exibir o nome do
+campo a ser preenchido ao invés de "enviado com sucesso". */
