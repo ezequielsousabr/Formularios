c95e3ec263c154e985b6910efd94684d07e3dbb2
Ezequiel L Sousa <ezequiel.sousa@yahoo.com>
2023-03-12 14:28:02 -0300

    Criando validaçoes - Updates

--- Temas/Checkout/CheckoutHtml/assets/js/default.js
+++ Temas/Checkout/CheckoutHtml/assets/js/default.js
@@ -1,47 +1,37 @@
 let CamposTag = document.getElementsByTagName('input');
+let EnviarPagamento = document.getElementById('pgamentsbuttonenv');
 
-const NumeroCartao = CamposTag[0].value;
-const Nome = CamposTag[1].value;
-const CupomDesconto = CamposTag[5].value;
+const NumeroCartao = new Number(CamposTag[0].value.length);
+const Nome = new String(CamposTag[1].value);
+const ValidaCupom = CamposTag[5].value;
+const CupomDesconto = new String("");
 
 function ErrorCampos(){
-    for(let cont = 0; cont <= CamposTag.length; cont++){
-        CamposTag[cont].style.border = "1px solid #FF033E";
 
-        console.log(cont)
-    }
-
-    return false;
+	for(let cont = 0; cont <= CamposTag.length; cont++){
+		CamposTag[cont].style.border = "1px solid red";
+	}
 }
 
-function CamposValidos(){
-    for(let cont = 0; cont <= CamposTag.length; cont++){
-        CamposTag[cont].style.border = "1px solid green";
+	if(CamposTag.value){
+		alert("Preencha todos os campos!!!")
+		ErrorCampos()
+	}else{
 
-        console.log(cont);
-    }
-}
-
-if(NumeroCartao == new String() || NumeroCartao == ""){
-    alert("Preencha o numero do seu Cartão de Crédito!")
-    ErrorCampos()
-}else{
-    CamposValidos()
-}
-
-if(Nome == new String() || Nome == new Number() || Nome == ""){
-    alert("Preencha seu Nome Completo!")
-    ErrorCampos()
-}else if(Nome.length <= 4){
-    alert("Seu nome precisa ter no minímo 5 letras!")
-    ErrorCampos()
-}else{
-    CamposValidos()
-}
-
-if(CupomDesconto.length !== 8){
-    alert("Seu cupom desconto está incorreto!")
-    ErrorCampos()
-}else{
-    CamposValidos()
-}
+		if(NumeroCartao <= 15 || NumeroCartao >= 17){
+			alert("Preencha o numero do seu Cartão de Crédito corretamente!!!")
+		}
+	
+		if(Nome === new Number() || Nome == ""){
+			alert("Preencha o campo nome corretamente! Ex: (A-Z,a-z)")
+		}else if(Nome.length <= 3){
+			alert("Seu nome precisa ter no minímo 4 letras!")
+		}
+	
+		if(CupomDesconto){
+			alert("Adicione seu cupom desconto!")
+			if(ValidaCupom.length !== 8){
+				alert("Cupom desconto incorreto!")
+			}
+		}
+	}
